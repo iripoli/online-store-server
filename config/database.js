509997518ca6ab57832a909c17ca4ce2e1239417
iripoli <iripoli96@gmail.com>
2020-05-22 18:23:08 +0200

    Connected the api to real db in server on mongodb atlas

--- config/database.js
+++ config/database.js
@@ -3,9 +3,9 @@ const express = require('express')
 
 const app = express()
 
-const DB_URI = process.env.DB_URI || 'mongodb://localhost/react-store'
+const DB_URI = process.env.DB_URI || 'mongodb+srv://iripoli:julian96@cluster0-12hh1.mongodb.net/online-store?retryWrites=true&w=majority'
 
 mongoose.connect(DB_URI, { useNewUrlParser: true })
-  .then(()=>console.log(`Connected to DB ${DB_URI}`))
+  .then(()=>console.log(`Connected to DB....`))
   .catch((err)=>console.log(`Error conecting to DB ${DB_URI}`, err.message))
 
